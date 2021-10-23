8e81b349bd67a253f8139f6757a3a50881756ceb
Paulo Ricardo Silva de Araújo <54222357+pkomander@users.noreply.github.com>
2021-10-23 11:06:10 -0300

    Exercicio criação de cronometro utilizando javaScript

--- regex/buscaRegex.js
+++ regex/buscaRegex.js
@@ -3,11 +3,11 @@
 let produtos = "3210232-B\n322032321-B\n321212121-C\n9885-D\n65560565-E\n9879908983213132-F";
 console.log(produtos);
 const regex = /[0-9]+[-][A-Z]/;
-//const resultado = produtos.match(regex);
-// console.log(resultado);
+const resultado = produtos.match(regex);
+console.log(resultado);
 
 
 //metodo Exec: recupera o resultado da correspondência de uma string com uma expressão regular
 //regex.exec(string)
-const resultado1 = regex.exec(produtos)
-console.log(resultado1);
+// const resultado1 = regex.exec(produtos)
+// console.log(resultado1);
